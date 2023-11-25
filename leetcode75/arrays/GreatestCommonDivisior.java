f002d8f8299f7649001dcf76bd8b368b491da7c0
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2023-11-25 12:22:06 +0530

    Create GreatestCommonDivisior.java

---
+++ leetcode75/arrays/GreatestCommonDivisior.java
@@ -0,0 +1,59 @@
+/*For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: "" */
+
+//code-
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+        int gcd = gcd(str1.length(),str2.length());
+        return str2.substring(0,gcd);
+    }
+
+public int gcd(int n1,int n2){
+    if (n2 == 0){
+        return n1;
+    }
+    return gcd(n2,n1 % n2);
+}
+}
+https://leetcode.com/problems/greatest-common-divisor-of-strings/submissions/1105835161?envType=study-plan-v2&envId=leetcode-75
+
+//modified code
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        int gcd = gcd(str1.length(), str2.length());
+        return str2.substring(0, gcd);
+    }
+
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+https://leetcode.com/problems/greatest-common-divisor-of-strings/submissions/1105839127?envType=study-plan-v2&envId=leetcode-75
